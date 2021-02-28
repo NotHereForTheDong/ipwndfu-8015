c54207eee886831bd6338c2ad764461dc73d13f3
NotHereForTheDong <kyletill35@hotmail.com>
2021-02-28 10:24:04 -0500

    Update A11-Nonce.py
    now users can define nonce with "-g 0xYourNonceHere"

--- A11-Nonce.py
+++ A11-Nonce.py
@@ -1,12 +1,28 @@
 import dfu
 import usbexec
 import sys
+import argparse
 import usb.core
 
+HOST2DEVICE = 0x21
+DEVICE2HOST = 0xA1
+DFU_DNLOAD = 1
+DFU_ABORT = 4
+
+parser = argparse.ArgumentParser(description='nonce setter', usage="./script.py -g 0xwhatever")
+parser.add_argument('-g', '--generator', help='Generator to set', nargs=1)
+args = parser.parse_args()
+if not args.generator:
+    exit('you didnt give a generator what the hell')
+
+nonce = hex(int(args.generator[0], 16))[2:]
+print(nonce)
+noncearray = bytearray.fromhex(nonce)
+
+#Patch Nonce
 device = dfu.acquire_device()
 serial_number = device.serial_number
 dfu.release_device(device)
 device = usbexec.PwnedUSBDevice()
 device.write_memory(0x2320b8000, '\x03\x02\x0a\x03')
-device.write_memory(0x2320b8028, '\x11\x11\x11\x11')
-device.write_memory(0x2320b802C, '\x11\x11\x11\x11')
+device.write_memory(0x2320b8028, noncearray)
